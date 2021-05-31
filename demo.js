3ee6f240c682b725fff8c2bb7c422d9de1d8670c
Adrian R <adrodriguez@transperfect.com>
2021-05-31 19:45:51 +0400

    AL-3491: +Updated v2 to v3 +Refactored async/await

--- demo.js
+++ demo.js
@@ -1,10 +1,17 @@
 var bt = require('./lib/bing-translate.js').init({
-    client_id: 'your_client_id', 
-    client_secret: 'your_client_secret'
-  });
+	client_id: 'your_client_id'
+});
 
 bt.translate('This hotel is located close to the centre of Paris.', 'en', 'ro', function(err, res){
-  console.log(err, res);
+	console.log(err, res);
+});
+
+bt.detect('Guess the language.', function(err, res){
+	console.log(err, res);
+});
+
+bt.getLanguagesForTranslate(function(err, res){
+	console.log(err, res);
 });
 
 // this example will throw an exception error because "se" is not recognised
